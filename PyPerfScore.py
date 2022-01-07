9f758de0fa6e770f8044297ab367a1a3697f7e93
Stehlampe2020 <94976382+Stehlampe2020@users.noreply.github.com>
2022-01-07 02:00:23 +0100

    Update PyPerfScore.py

--- PyPerfScore.py
+++ PyPerfScore.py
@@ -4,23 +4,63 @@ import sys
 import time as t
 
 def run_test(is_graphical = False):
-    runs_max = 1000000
-    runs_passed = 0
-    number = 0
-    time_at_start = t.time()
-    while runs_passed <= runs_max:
+    if is_graphical == False:
+        runs_max = 1000000
+        runs_passed = 0
+        number = 0
+        time_at_start = t.time()
+        while runs_passed <= runs_max:
             if runs_passed < runs_max:
-                    print(number, end="\r")
-                    number +=1
-                    runs_passed += 1
+                print(number, end="\r")
+                number +=1
+                runs_passed += 1
             elif runs_passed == runs_max:
-                    print(number)
-                    time_at_finish = t.time()
-                    time_to_complete = time_at_finish - time_at_start
-                    print("Your computer needed " + str(round(time_to_complete, 3)) + " seconds to count from zero to " + str(runs_max) + " as fast as it could.")
-                    score = runs_max / time_to_complete
-                    print("So the score is " + str(round(score)) + ".")
-                    exit()
+                print(number)
+                time_at_finish = t.time()
+                time_to_complete = time_at_finish - time_at_start
+                print("Your computer needed " + str(round(time_to_complete, 3)) + " seconds to count from zero to " + str(runs_max) + " as fast as it could.")
+                score = runs_max / time_to_complete
+                print("So the score is " + str(round(score)) + ".")
+                exit()
+    elif is_graphical == True:
+        print("As a placeholder the test will still run in command-line mode but I'm working on the graphical variant.")
+        runs_max = 1000000
+        runs_passed = 0
+        number = 0
+        time_at_start = t.time()
+        while runs_passed <= runs_max:
+            if runs_passed < runs_max:
+                print(number, end="\r")
+                number +=1
+                runs_passed += 1
+            elif runs_passed == runs_max:
+                print(number)
+                time_at_finish = t.time()
+                time_to_complete = time_at_finish - time_at_start
+                print("Your computer needed " + str(round(time_to_complete, 3)) + " seconds to count from zero to " + str(runs_max) + " as fast as it could.")
+                score = runs_max / time_to_complete
+                print("So the score is " + str(round(score)) + ".")
+                exit()
+        
+    else:
+        print("""A bug set the value for "is_graphical" to an invalid value. Running test in command-line mode:""")
+        runs_max = 1000000
+        runs_passed = 0
+        number = 0
+        time_at_start = t.time()
+        while runs_passed <= runs_max:
+            if runs_passed < runs_max:
+                print(number, end="\r")
+                number +=1
+                runs_passed += 1
+            elif runs_passed == runs_max:
+                print(number)
+                time_at_finish = t.time()
+                time_to_complete = time_at_finish - time_at_start
+                print("Your computer needed " + str(round(time_to_complete, 3)) + " seconds to count from zero to " + str(runs_max) + " as fast as it could.")
+                score = runs_max / time_to_complete
+                print("So the score is " + str(round(score)) + ".")
+                exit()
 
 def load_gui(force_to_run = False):
     gui = Tk()
@@ -33,9 +73,9 @@ def load_gui(force_to_run = False):
     if force_to_run == True:
         print("Ok, the GUI will be loaded if possible. Note that this will probably cause a traceback.\nThe creator of this code is not responsible for any damage caused by this part of the code itself or its use!")
         messagebox.showinfo("Welcome!", """The GUI isn't fully implemented just yet!\nAnyway, since you run this program with "--force-gui" it will try to load the full GUI. Note that this will probably cause a traceback.\nThe creator of this code is not responsible for any damage caused by this part of the code itself or its use!""")
-        #graphical code goes here
+        run_test(True)
         print("End of code.")
-    
+
 def ask_for_args():
     args = sys.argv
     if len(args) == 2:
