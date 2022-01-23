f0214d9d5bf8ac2041439e2e1b2d618b0f383e2f
Stehlampe2020 <94976382+Stehlampe2020@users.noreply.github.com>
2022-01-23 15:21:32 +0100

    Update 0.2.3-alpha
    Changes:   
    -> version: 0.2.2-alpha --> 0.2.3-alpha   
    -> fixed some typos in the in-code documentation   
    -> added the command-line option "--debug", which makes the program go into interactive-shell-mode after finishing or error occurrence   
    -> added behaviour: if two arguments are given, the program goes into interactive-shell-mode after finishing or error occurrence   
    -> optimized the text output of the program

--- PyPerfScore.py
+++ PyPerfScore.py
@@ -2,9 +2,12 @@
 
 import sys
 import time as t
+import code
+import readline
+import rlcompleter
 
 global version
-version = "0.2.2-alpha"
+version = "0.2.3-alpha"
 print("You are now running version " + version + """ of PyPerfScore. To check for newer versions, please check out "https://github.com/Stehlampe2020/PyPerfScore".""")
 
 def run_test(is_graphical = False):
@@ -25,34 +28,34 @@ def run_test(is_graphical = False):
                             score = cycles / time_to_complete   # Compute the score: divide the time the counting took by the number of cycles that the loop was run. 
                         except DivisionByZero:
                             print("Your computer seems to be too fast to run this test with a target number of", cycles, """cycles. Maybe multiply the value of the variable "cycles"?""")
-                            return 1    # Return a 1 as a return code for this function. Later I will add a system to handle return values, for this reason I add the codes themselves already now.
+                            return 1    # Return a 1 as a return code for this function. Return code 1 means "DivisionByZeroError".
                         print("So the score is " + str(round(score)) + ".") # Print the score in a nice form and rounded to a whole number to the terminal.
-                        return 0    # Return a zero to signal the return value handler that everything went right. The return value handler will be added later. 
+                        return 0    # Return a zero to signal the return value handler that everything went right.
     elif is_graphical == True:
         load_gui(True)
 
 def load_gui(force_to_run = False): # This function is for loading the GUI of the program but I think I will merge this function with the is_graphical=True part of run_test() later.
-    gui = Tk()  # Define a windoe with the ID "gui".
+    gui = Tk()  # Define a window with the ID "gui".
     gui.title("PyPerfScore GUI")    # Give the window with the ID "gui" a title. 
     if force_to_run == False:
         messagebox.showinfo("Welcome!", """The GUI isn't implemented just yet, please check for new versions that have this feature implemented!\nClicking "OK" below will run the test anyway in command-line mode.""")
-        print("The GUI isn't fully implemented yet, anyway, the test will be performed in command-line mode below:")    # Print a shortened version of the same message to the terminal for the case that the window doesn't show up or someone wants to understand what happened just based on the terminal output.
-        gui.destroy()   # Destroy (close) the window with the ID "gui" after "OK" has been clicked in the messagebox above. 
-        run_test(False) # Run the test itself in command-line mode. (Why I had this one set to True before? I don't know!)
+        print("The GUI isn't fully implemented yet, anyway, the test will be performed in command-line mode below:")    # Print a shortened version of the same message to the terminal for the case that the window doesn't show up or someone wants to figure out what happened just based on the terminal output.
+        gui.destroy()   # Destroy (close) the window with the ID "gui" after "OK" has been clicked in the messagebox that's defined above. 
+        run_test(False) # Run the test itself in command-line mode. 
     if force_to_run == True:
         print("Ok, the GUI will be loaded if possible. Note that this will probably cause a traceback.\nThe creator of this code is not responsible for any damage caused by this part of the code itself or its use!") # Warn the user in the command-line that errors could occur here. If the window doesn't show up or somehow freezes before displaying the warning graphically the user is warned. 
         messagebox.showwarning("Welcome!", """The GUI isn't fully implemented just yet!\nAnyway, since you run this program with "--full-gui" it will try to load the full GUI. Note that this will probably cause a traceback.\nThe creator of this code is not responsible for any damage caused by this part of the code itself or its use!""")  # Show the same warning, but in GUI mode.
         #graphical code goes here
         messagebox.showinfo("Done!", """Perfect, the part of the code that is still in development seems to not have crashed anything! If you like to submit issues that occurred, please go to "https://github.com/Stehlampe2020/PyPerfScore/issues" to report them.\n\n*End of Code*""")  # Congratulations! If you see that message the program didn't crash! 
-        gui.destroy()   # Destroy (close) the window with the ID "gui" after "OK" has been clicked in the messagebox above. 
-        print("""Perfect, the part of the code that is still in development seems to not have crashed anything! If you like to submit issues that occurred, please go to "https://github.com/Stehlampe2020/PyPerfScore/issues" to report them.\n*End of code*""")   # Congratulating also in command-line mode if (for some reason) the graphical message doesn't get displayed. 
-        return 0    # Return a zero to signal the return value handler that everything went right. The return value handler will be added later. 
+        gui.destroy()   # Destroy (close) the window with the ID "gui" after "OK" has been clicked in the messagebox that's defined above. 
+        print("""Perfect, the part of the code that is still in development seems to not have crashed anything! If you would like to submit issues that occurred, please go to "https://github.com/Stehlampe2020/PyPerfScore/issues" to report them.\n*End of code*""")   # Congratulating also in command-line mode if (for some reason) the graphical message didn't get displayed. 
+        return 0    # Return a zero to signal the return value handler that everything went right (as far as we can know). 
     
 def ask_for_args():
     args = sys.argv
     if len(args) == 2:  # If two arguments are given, do one of the following things: 
         # The program name is also counted as an argument when using sys.argv to ask for the command-line arguments. 
-        # Because computers are counting from zero up and not from one, we ask for argument 1 (which is the second argument in sys.argv) to get the first argument AFTER the program name. If we asked for the first argument we would get the program name but that doesn't interest us at all at the moment.
+        # Because computers are counting from zero up and not from one, we ask for argument 1 (which is the second argument in sys.argv) to get the first argument AFTER the program name. If we asked for the first argument we would get the program name but that doesn't interest us at all.
         if args[1] == "--gui":  # If the second argument is --gui we load the GUI:
             print("Loading GUI...")
             load_gui()
@@ -64,19 +67,73 @@ def ask_for_args():
         elif args[1] == "--cmdline":    # If the second argument is --cmdline we simply run the test in command-line mode. This option is useless for now but will be useful when I switch the default value for is_graphical  for run_test() from False to True. 
             print("Running in command-line mode...")
             run_test(False)
-            return 0    # This time we didn't fail.
+            return 0    # This time we didn't fail. Signaling that to the return code handler. 
         else:
-            print("""Incorrect argument used!\nThe only arguments that will be accepted are "--cmdline", "--gui" and "--full-gui".\n "--cmdline" will run this program in command-line mode, "--gui" will only load the parts of the GUI that are fully implemented by now and "--full-gui" will try to run the full GUI even though some parts may cause crashes.""")
+            print("""Incorrect argument used!\nThe only arguments that will be accepted are "--cmdline", "--gui", "--full-gui" and "--debug".\n "--cmdline" will run this program in command-line mode, "--gui" will only load the parts of the GUI that are fully implemented by now and "--full-gui" will try to run the full GUI even though some parts may cause crashes. "--debug" enters an interactive shell if any error occurrs or the program finishes (a bit like the "-i" option for the "python3"-command).""")
             exit()
             return 2    # This time fe failed. Returning a 2 for "wrong argument".
-    elif len(args) >= 3:    # Warn the user if too many arguments were used and the quit the program. 
-        print("Too many arguments!\n(This program only takes one...)")
+    elif len(args) == 3:
+        if args[1] == "--gui" and args[-1] == "--debug":  # If the second argument is --gui and the last argument is --debug we load the GUI:
+            print("Loading GUI...")
+            try:
+                load_gui()
+            except KeyboardInterrupt:
+                print("\rKeyboardInterrupt")
+            except:
+                print("Something failed while the GUI was loading or active!")
+            finally:
+                vars = globals()
+                vars.update(locals())
+                readline.set_completer(rlcompleter.Completer(vars).complete)
+                readline.parse_and_bind("tab: complete")
+                code.InteractiveConsole(vars).interact()
+            return 0    # This time we didn't fail.
+        elif args[1] == "--full-gui" and args[-1] == "--debug":   # If the second argument is --full-gui we load the GUI with the input value True for force_to_run to load even the unstable parts of the GUI. This option will be commented out as soon as I finish writing the GUI and the program gets into the release phase.
+            print("Loading GUI...")
+            try:
+                load_gui(True)
+            except KeyboardInterrupt:
+                print("\rKeyboardInterrupt")
+            except:
+                print("Something failed while the GUI was loading or active!")
+            finally:
+                vars = globals()
+                vars.update(locals())
+                readline.set_completer(rlcompleter.Completer(vars).complete)
+                readline.parse_and_bind("tab: complete")
+                code.InteractiveConsole(vars).interact()
+            return 0    # This time we didn't fail.
+        elif args[1] == "--cmdline" and args[-1] == "--debug":    # If the second argument is --cmdline we simply run the test in command-line mode. This option is useless for now but will be useful when I switch the default value for is_graphical  for run_test() from False to True. 
+            print("Running in command-line mode...")
+            try:
+                run_test(False)
+            except KeyboardInterrupt:
+                print("\rKeyboardInterrupt")
+            except:
+                print("Something failed while the test was loading or running!")
+            finally:
+                vars = globals()
+                vars.update(locals())
+                readline.set_completer(rlcompleter.Completer(vars).complete)
+                readline.parse_and_bind("tab: complete")
+                code.InteractiveConsole(vars).interact()
+            return 0    # This time we didn't fail. Signaling that to the return code handler. 
+        else:
+            print("""Incorrect argument used!\nThe only arguments that will be accepted are "--cmdline", "--gui", "--full-gui" and "--debug".\n "--cmdline" will run this program in command-line mode, "--gui" will only load the parts of the GUI that are fully implemented by now and "--full-gui" will try to run the full GUI even though some parts may cause crashes. "--debug" enters an interactive shell if any error occurrs or the program finishes (a bit like the "-i" option for the "python3"-command).""")
+            vars = globals()
+            vars.update(locals())
+            readline.set_completer(rlcompleter.Completer(vars).complete)
+            readline.parse_and_bind("tab: complete")
+            code.InteractiveConsole(vars).interact()
+            return 2    # This time fe failed. Returning a 2 for "wrong argument".
+    elif len(args) >= 4:    # Warn the user if too many arguments were used and the quit the program. 
+        print("Too many arguments!\n(This program only takes up to two...)")
         exit()
         return 3    # This time we failed. Returning a 3 for "too many arguments were used".
     elif len(args) == 1:    # If there's only one argument given (the name of the program, like described in line 51 of the source code) we warn the user that no arguments (in the way we think of them when entering them) were detected and run the test in default mode. For now, that's command-line mode but that'll change later.
         print("""No command-line options were detected, running in default mode.\nIf you intended to run this program in graphical mode, press [Ctrl] + [C] now to cancel and re-run this program with the argument "--gui".""")
         run_test()
-        return 0    # This time we didn't fail. We were just running in a pre-defines mode instead of a user-specified one.
+        return 0    # This time we didn't fail. We were just running in a pre-defined mode instead of a user-specified one.
     else:   # If somehow less than zero arguments (in the way we think of them when entering them) were detected we warn the user and ask to report the issue.
         print("An unknown error occurred while checking for command-line options, please report this behaviour and the conditions under which this error occurred!")
         return 4    # This time we failed. Returning a 4 for "unknown error while checking for command-line arguments".
