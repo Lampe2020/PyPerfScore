b67517dd6bf423b40d0864d48544e200df6eb991
Stehlampe2020 <94976382+Stehlampe2020@users.noreply.github.com>
2022-08-18 01:47:03 +0200

    Update ppsupdater.py
    ...to add two options:
    * `--ver`: If you specify a version using `--ver=vSomever` the target version will be downloaded, in this example vSomever.
    * `--path`: If you specify a local path using `--path=/home/user/PyPerfScore` the download will not happen in the current directory but rather in the requested path, in this example /home/user/PyPerfScore.

--- ppsupdater.py
+++ ppsupdater.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+import os
 import requests
 
 if __name__ not in ("__main__", "ppsupdater"):
@@ -17,6 +18,19 @@ except Exception as e:
     print("Error: could not import language into ppsupdater! ({}: {error_message})".format(type(e).__name__, error_message=e))
     exit("failed_to_import_language")
 
+from sys import argv
+args = argv.copy()
+del argv
+
+def argument(argument_to_search_for):
+    if not argument_to_search_for in args:
+        for arg in args:
+            if arg[0:len(argument_to_search_for)+1] == argument_to_search_for+"=":
+                return arg.split("=", 1)[1]
+        return False
+    else:
+        return True
+
 try:
     with open("ver") as version_file:
         version = version_file.read().strip()
@@ -24,8 +38,33 @@ except OSError:
     print(could_not_import_ver)
     version = "0.0.0"
 
-base_path_remote = "https://raw.githubusercontent.com/Stehlampe2020/PyPerfScore/main/" # The remote base path. → "{}some_file".format(base_path_remote)
+base_path_remote = "https://raw.githubusercontent.com/Stehlampe2020/PyPerfScore/{release}/" # The remote base path. → "{}some_file".format(base_path_remote)
 base_path_local = "./" # The local base path. "./" refers to the current working directory. Later I will make a detector for the absolute path. → "{}some_file".format(base_path_local)
+
+requested_ver = argument("--ver")
+requested_path = argument("--path")
+
+if requested_ver:
+    if requests.get(base_path_remote.format(release=str(requested_ver))+"files.list").status_code == 200:
+        base_path_remote = base_path_remote.format(release=requested_ver)
+    else:
+        print(strings[5].format(release=requested_ver))
+        if argument("--exit"):
+            exit()
+        base_path_remote = base_path_remote.format(release="main")
+else:
+    base_path_remote = base_path_remote.format(release="main")
+
+if requested_path:
+    if not requested_path[-1] == "/":
+        requested_path += "/"
+    if os.path.isdir(requested_path):
+        try:
+            os.mkdir("".join([requested_path, "lang"]))
+        except FileExistsError:
+            pass # If the directory already exists, just go on.
+        base_path_local = requested_path
+        version = "--path" # Tell the program that it's not installed yet where --path specifies it. This works by changing the version string to one that's definetly not the target version!
 
 try:
     latest_ver = requests.get("{}ver".format(base_path_remote)).text.strip()
@@ -48,8 +87,8 @@ if version != latest_ver:
         prev_filename = filename
     print(strings[3].format(100.0, strings[4]))
     
-    import os
-    os.chmod("PyPerfScore.py", 0o775) # os.system("chmod +x ./PyPerfScore.py") # Make `PyPerfScore.py` executable.
+    os.chmod("".join([base_path_local, "ppsupdater.py"]), 0o775) # Make `ppsupdater.py` executable.
+    os.chmod("".join([base_path_local, "PyPerfScore.py"]), 0o775) # Make `PyPerfScore.py` executable.
     
     raise SystemExit(strings[0]) # Notify the user that a restart of PyPerfScore is necessary and then quit.
 else:
